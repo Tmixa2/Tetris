526cce2b223afd914e2e9e22f17c5261042ae506
Tmixa2 <118190458+Tmixa2@users.noreply.github.com>
2024-05-05 17:26:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ init()
 scr = display.set_mode((700, 500))
 game = True
 bg = transform.scale(image.load("fon.jpg"), (700, 500))
-sources = ["1x1red.jpg", "1x1blue.png", "1x1_green.png"]
+sources = ["1x1red.jpg", "1x1blue.png", "1x1green.png"]
 frame = time.Clock()
 speed_flag = 0
 global fallen
@@ -13,7 +13,6 @@ fallen = []
 def rect_collide(rect1, rect2):
     if rect2.rect.right > rect1.rect.left and rect2.rect.left < rect1.rect.right and rect2.rect.bottom > rect1.rect.top and rect2.rect.top < rect1.rect.bottom:
         return True
-
 class gamesprites(sprite.Sprite):
     def __init__(self, imga, speed, px, py, x,y, sections):
         self.img = transform.scale(image.load(imga), (px, py))
@@ -45,15 +44,16 @@ class block(gamesprites):
         if keys[K_a] and self.rect.bottomleft[0] >= 240:
             if self.can_move:
                 self.rect.x -= 5
+    def rotate1(self):
+        rotated_image = transform.rotate(self.img, 90)
+        x = self.rect.x
+        y = self.rect.y
+        self.img = rotated_image
+        self.rect = self.img.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+                
 
-        if keys[K_SPACE] and self.can_move == True:
-            rotated_image = transform.rotate(self.img, 90)
-            x = self.rect.x
-            y = self.rect.y
-            self.img = rotated_image
-            self.rect = self.img.get_rect()
-            self.rect.x = x
-            self.rect.y = y
     def increase_speed(self):
         if self.velocity < 7:
             if self.rect.y > 0:
@@ -81,10 +81,13 @@ for j in range(50):
         tile = block(i, 4, 25, 50, 350, (j*-500), 4)
     blocks.append(tile)
 while game:
+    rotate = False
     for i in event.get():
         if i.type == QUIT:
             game = False
         if i.type == KEYDOWN:
+            if i.key == K_SPACE:
+                rotate = True
             if i.key == K_w:
                 speed_flag = 1
         if i.type == KEYUP:
@@ -97,11 +100,12 @@ while game:
     scr.blit(bg, (0, 0))
     for i in blocks:
         i.check_for_keypress()
+        if rotate:
+            i.rotate1()
         i.show()
         if i.fall():
             fallen.append(i)
             blocks.remove(i)
-
         if speed_flag == 1:
             i.increase_speed()
         if speed_flag == 0:
@@ -112,6 +116,11 @@ while game:
             for j in blocks:
                 if rect_collide(i, j):
                     y = i.get_y()
+                    if i.rect.top < j.rect.bottom:
+                        if j.rect.centerx > i.rect.right:
+                            j.rect.left = i.rect.right
+                        elif j.rect.centerx < i.rect.left:
+                            j.rect.right = i.rect.left
                     j.ret_y(y)
                     fallen.append(j)
                     blocks.remove(j)
