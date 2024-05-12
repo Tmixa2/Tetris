4af0612111d1ef6c9812a85b0e5eadf6b32731ff
Tmixa2 <118190458+Tmixa2@users.noreply.github.com>
2024-05-12 17:28:46 +0500

    Update main.py

--- main.py
+++ main.py
@@ -5,45 +5,52 @@ init()
 scr = display.set_mode((700, 500))
 game = True
 bg = transform.scale(image.load("fon.jpg"), (700, 500))
-sources = ["1x1blue.png"]#["1x1red.jpg", "1x1blue.png", "1x1_green.png"]
+sources = ["1x1red.jpg", "1x1blue.png"]#, "1x1_green.png"
 frame = time.Clock()
 speed_flag = 0
 global fallen
 fallen = []
-def rect_collide(rect1, rect2):
-    if rect2.rect.right > rect1.rect.left and rect2.rect.left < rect1.rect.right and rect2.rect.bottom > rect1.rect.top and rect2.rect.top < rect1.rect.bottom or rect2.part_rect.right > rect1.rect.left and rect2.part_rect.left < rect1.rect.right and rect2.part_rect.bottom > rect1.rect.top and rect2.part_rect.top < rect1.rect.bottom or rect2.part_rect.right > rect1.part_rect.left and rect2.part_rect.left < rect1.part_rect.right and rect2.part_rect.bottom > rect1.part_rect.top and rect2.part_rect.top < rect1.part_rect.bottom or rect2.rect.right > rect1.part_rect.left and rect2.rect.left < rect1.part_rect.right and rect2.rect.bottom > rect1.part_rect.top and rect2.rect.top < rect1.part_rect.bottom:
-        return True
+
 class gamesprites(sprite.Sprite):
-    def __init__(self, imga, speed, px, py, x,y, sections, few_sections):
+    def __init__(self, imga, speed, px, py, x,y, sections):
         self.img = transform.scale(image.load(imga), (px, py))
         self.rect = self.img.get_rect()
         self.rect.x = x
-        self.few_sections = few_sections
         self.rect.y = y
         self.velocity = speed
-        self.rot = False
         self.gravity = 3
         self.can_move = True
-        if self.few_sections:
-            self.part_img = transform.scale(image.load(imga), (25, 25))
-            self.part_rect = self.part_img.get_rect()
+        self.more_sections = False
+        self.rot = False
+        if "blue" in imga:
+            self.more_sections = True
+            self.section_img = transform.scale(image.load(imga), (25, 25))
+            self.rect2 = self.section_img.get_rect()
+            self.rect2.left = self.rect.right
+            self.rect2.top = self.rect.top
     def show(self):
         scr.blit(self.img, (self.rect.x, self.rect.y))
-        if self.few_sections:
-            scr.blit(self.part_img, (self.part_rect.x, self.part_rect.y))
-
+        if self.more_sections:
+            scr.blit(self.section_img, (self.rect2.x, self.rect2.y))
 
 class block(gamesprites):
     def fall(self):
-        if self.few_sections:
-            if self.rot:
-                self.part_rect.top = self.rect.bottom
-                self.part_rect.right = self.rect.right
+        if self.can_move and self.more_sections:
+            if self.rect.bottom < 463 and self.rect2.bottom:
+                self.rect.y += self.velocity
+                if not self.rot:
+                    self.rect2.left = self.rect.right
+                    self.rect2.top = self.rect.top
+                else:
+                    self.rect2.top = self.rect.bottom
+                    self.rect2.right = self.rect.right
             else:
-                self.part_rect.left = self.rect.right
-                self.part_rect.top = self.rect.top
+                self.can_move = False
+                return True
+        return False            
+
         if self.can_move:
-            if self.rect.bottom < 463 and self.part_rect.bottom < 463:
+            if self.rect.bottom < 463:
                 self.rect.y += self.velocity
             else:
                 self.can_move = False
@@ -66,6 +73,13 @@ class block(gamesprites):
         self.rect = self.img.get_rect()
         self.rect.x = x
         self.rect.y = y
+        if not self.rot:
+            if self.more_sections:
+                self.rect2.top = self.rect.bottom
+                self.rect2.right = self.rect.right
+                self.rot = True
+        else:
+            self.rot = False
                 
 
     def increase_speed(self):
@@ -77,25 +91,19 @@ class block(gamesprites):
             self.velocity -= 3
     def set_move(self):
         self.can_move = False
-    def ret_y(self, y):
-        if y - self.rect.bottom <= 3:
-            pass
-        else:
-            self.rect.y += (y - self.rect.bottom)
-    def get_y(self):
-        return self.rect.top
+
 
 blocks = []
 for j in range(50):
     i = choice(sources)
     if "red" in i:
-        tile = block(i, 4, 25, 25, 350, (j*-500), 1, False)
+        tile = block(i, 4, 25, 25, 350, (j*-500), 1)
         blocks.append(tile)
     if "blue" in i:
-        tile = block(i, 4, 25, 75, 350, (j*-500), 3, True)
+        tile = block(i, 4, 25, 75, 350, (j*-500), 3)
         blocks.append(tile)
     if "green" in i:
-        tile = block(i, 4, 25, 50, 350, (j*-500), 4, False)
+        tile = block(i, 4, 25, 50, 350, (j*-500), 4)
         blocks.append(tile)
 while game:
     rotate = False
@@ -110,20 +118,12 @@ while game:
         if i.type == KEYUP:
             if i.key == K_w:
                 speed_flag = 0
-        if i.type == KEYDOWN:
-            pass
-
 
     scr.blit(bg, (0, 0))
     for i in blocks:
         i.check_for_keypress()
         if rotate:
             i.rotate1()
-            if i.few_sections:
-                if i.rot:
-                    i.rot = False
-                else:
-                    i.rot = True
         i.show()
         if i.fall():
             fallen.append(i)
@@ -136,23 +136,29 @@ while game:
         for i in fallen:
             i.show()
             for j in blocks:
-                if rect_collide(i, j):
-                    if i.rect.top < j.rect.bottom:
-                        if j.rect.centerx > i.rect.right:
-                            j.rect.left = i.rect.right
-                        elif j.rect.centerx < i.rect.left:
-                            j.rect.right = i.rect.left
-                    if j.few_sections:
-                        if j.part_rect.bottom > i.rect.top:
-                            j.part_rect.bottom = i.rect.top
-                            j.rect.bottom = j.part_rect.top
-                        else:
-                            j.rect.bottom = i.rect.top
-                            j.part_rect.left = j.rect.right
-                            j.part_rect.top = j.rect.top
+                try:
+                    if j.rect.bottom > i.rect.top and j.rect.top < i.rect.bottom and j.rect.right > i.rect.left and j.rect.left < i.rect.right:
+                        if i.rect.top < j.rect.bottom:
+                            if j.rect.centerx > i.rect.right:
+                                j.rect.left = i.rect.right
+                            elif j.rect.centerx < i.rect.left:
+                                j.rect.right = i.rect.left
                         fallen.append(j)
-                    blocks.remove(j)
-                    j.set_move()
+                        blocks.remove(j)
+                    if j.more_sections:
+                        if j.rect2.bottom > i.rect.top and j.rect2.top < i.rect.bottom and j.rect2.right > i.rect.left and j.rect2.left < i.rect.right:
+                            if j.rect2.bottom > i.rect.top:
+                                if j.rect2.bottom > i.rect.top:
+                                    j.rect2.bottom = i.rect.top
+                                if j.rect2.right > i.rect.left:
+                                    j.rect2.right = i.rect.left
+                                if j.rect2.left < i.rect.right:
+                                    j.rect2.left = i.rect.right
+                            fallen.append(j)
+                            blocks.remove(j)
+                        j.set_move()
+                except:
+                    pass
 
 
     display.update()
